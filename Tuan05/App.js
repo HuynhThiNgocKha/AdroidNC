1bb100ed6386679b3a91a20c9faae5e8a53cbc6b
Your Name <you@example.com>
2024-03-15 17:25:29 +0700

    update Tuan05

---
+++ Tuan05/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Bai1 from './Screen/Bai1';
+import Bai2 from './Screen/Bai2';
+import Bai3 from './Screen/Bai3';
+
+
+export default function App() {
+  return (
+    <Bai1/>
+    // <Bai2/>
+    // <Bai3/>
+  );
+
+}
