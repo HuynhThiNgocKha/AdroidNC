5fd5c35eea08cb12d4408d640c9bb31a6cfabcfd
Your Name <you@example.com>
2024-04-24 00:03:18 +0700

    Tuan10

--- Tuan10/App.js
+++ Tuan10/App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import Home from './screen/Home';
+import Inbox from './screen/Inbox';
+import Settings from './screen/Settings';
+import Appointments from './screen/Appointments';
 
+const stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator screenOptions={{headerShown:false}}>
+        <stack.Screen name="Home" component={Home} />
+        <stack.Screen name="Inbox" component={Inbox} />
+        <stack.Screen name="Settings" component={Settings} />
+        <stack.Screen name="Appointments" component={Appointments} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
